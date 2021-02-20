12c4bbc1d6acc1964907f8b316257d61422fdcb0
Connor Leech <connorleech@gmail.com>
2021-02-20 14:49:57 -0800

    url fix take 2

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,10 +1,2 @@
 // Alpine.js handles the show/hide of the mobile nav
 import 'alpinejs'
-
-// make sure we can go to urls without slash at the end
-// https://flaviocopes.com/netlify-fix-trailing-slash-rewrite/
-(function() {
-    if (!location.href.endsWith('/')) {
-        window.location = location.href + '/'
-    }
-}());
