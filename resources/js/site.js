9bdf3865f2e82bf7d6cd090fc4fe7b691b6c3f75
Connor Leech <connorleech@gmail.com>
2021-02-20 14:12:30 -0800

    try out removing slash from urls

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,2 +1,10 @@
 // Alpine.js handles the show/hide of the mobile nav
 import 'alpinejs'
+
+// make sure we can go to urls without slash at the end
+// https://flaviocopes.com/netlify-fix-trailing-slash-rewrite/
+(function() {
+    if (!location.href.endsWith('/')) {
+        window.location = location.href + '/'
+    }
+}());
